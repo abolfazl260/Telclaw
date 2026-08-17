0579125a1573f1089e9a8da083086aecdc4ddd0a
Abolfazl Rezaie <abolfazl2600@gmail.com>
2026-08-17 19:32:01 +0330

    refactor: extract account application service

---
+++ services/account_service.py
@@ -0,0 +1,26 @@
+"""Application service for Telegram account/client lifecycle."""
+
+import sessions_manager
+
+
+class AccountService:
+    def list_accounts(self):
+        return sessions_manager.get_active_accounts()
+
+    def create_client(self, account_name):
+        return sessions_manager.create_client(account_name)
+
+    async def connect(self, account_name):
+        client = self.create_client(account_name)
+        await client.connect()
+        if not await client.is_user_authorized():
+            await client.disconnect()
+            raise PermissionError(f"Telegram session '{account_name}' is not authorized")
+        return client
+
+    async def register(self, session_name):
+        return await sessions_manager.register_new_account(session_name)
+
+    async def disconnect(self, client):
+        if client is not None:
+            await client.disconnect()
